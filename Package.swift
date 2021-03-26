d3f25912dea34a99197f6348ac93137089516e64
Quentin Yann PIDOUX <quentin.pidoux@gmail.com>
2021-03-26 21:30:51 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,16 +1,20 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "PLogger",
-    platforms: [ .macOS(.v10_10), .iOS(.v10), .tvOS(.v9), .watchOS(.v3)],
-    products: [
-        .library(name: "PLogger", targets: ["PLogger"])
-    ],
-    targets: [
-        .target(name: "PLogger"),
-        .testTarget(name: "PLoggerTests", dependencies: ["PLogger"])
-    ]
+  name: "PLogger",
+  platforms: [ .macOS(.v10_10),
+               .iOS(.v10),
+               .tvOS(.v9),
+               .watchOS(.v3)],
+  products: [
+    .library(name: "PLogger", targets: ["PLogger"])
+  ],
+  dependencies: [],
+  targets: [
+    .target(name: "PLogger", dependencies: []),
+    .testTarget(name: "PLoggerTests", dependencies: ["PLogger"])
+  ]
 )
